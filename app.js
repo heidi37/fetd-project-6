42992ef77b00fe79af4322cbd882281e6cc5833f
Heidi <heidi37@users.noreply.github.com>
2019-09-11 21:51:35 -0400

    functions for choosing phrase and splitting into letters and displaying in HTML working

--- app.js
+++ app.js
@@ -4,9 +4,53 @@ const buttonStart = document.querySelector('.btn__reset');
 const mainContainer = document.querySelector('.main-container');
 const overlay = document.getElementById('overlay');
 
-console.log(mainContainer);
+
+
+var phrases = [
+    'GAME OF THRONES',
+    'ORANGE IS THE NEW BLACK',
+    'SIX FEET UNDER',
+    'THE OFFICE',
+    'SMALLVILLE']
 
 buttonStart.addEventListener('click', () => {
     mainContainer.removeChild(overlay);
 });
 
+function getRandomPhraseAsArray(arr){
+    //randomly choose a phrase from the array
+    var randomNumber = Math.floor(Math.random() * 5);
+
+    //split phrase into a new array of characters
+    var splitPhrase = phrases[randomNumber].split('');
+
+    //return the new array
+    return splitPhrase;
+}
+
+
+
+function addPhraseToDisplay(arr){
+    
+    // loop through splitPhrase array
+    for (i = 0; i <arr.length; i++){
+    // for each character in the array create an li
+    // append li to `#phrase ul`
+    const phraseDiv = document.querySelector('#phrase');
+    const ul = phraseDiv.querySelector('ul');
+    const li = document.createElement('li')
+
+    ul.appendChild(li);
+    li.textContent = arr[i];
+
+    // // if character is a letter, should add the class "letter to the li"
+        if (arr[i] !==' ') {
+            li.className = 'letter';
+        } else {
+            li.className = 'space';
+        }
+    }
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
